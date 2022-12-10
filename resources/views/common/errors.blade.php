b61eeeaea313a433708b8bfb8a1096c18e9158a7
MATSUYAMA09 <donkungonmo@outlook.jp>
2022-12-10 16:01:49 +0900

    first commit

---
+++ resources/views/common/errors.blade.php
@@ -0,0 +1,15 @@
+@if (count($errors) > 0)
+{{-- フォームのエラーリスト --}}
+<div class="alert aleat-danger">
+    <strong>エラーが発生しました</strong>
+
+    <br><br>
+
+    <ul>
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+
+@endif
